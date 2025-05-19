0c77eb388875788fd956e489a245eddf526ec270
RedaKaafarani1 <kaafaranireda@gmail.com>
2025-05-19 23:24:35 +0200

    the service now accepts data path through env

--- internal/config/paths.go
+++ internal/config/paths.go
@@ -1,12 +1,21 @@
 package config
 
 import (
+	"os"
 	"path/filepath"
 )
 
 // DataDir is the base directory for all data files
-// var DataDir = filepath.Join("../..", "data")
-var DataDir = filepath.Join(".", "data")
+var DataDir string
+
+func init() {
+	// Get data directory from environment variable, default to "./data" if not set
+	if envDataDir := os.Getenv("DATA_DIR"); envDataDir != "" {
+		DataDir = envDataDir
+	} else {
+		DataDir = filepath.Join(".", "data")
+	}
+}
 
 // GetDataFilePath returns the full path for a data file given its name
 func GetDataFilePath(filename string) string {
